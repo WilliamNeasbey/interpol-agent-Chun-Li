792d8c579c2813e9f1a70012603402c1def5327a
MicroSillyWilly <1025946@student.sae.edu.au>
2023-08-10 11:54:20 +1000

    added customization stuff

--- Assets/Scripts/CostumeManager.cs
+++ Assets/Scripts/CostumeManager.cs
@@ -4,6 +4,9 @@ public class CostumeManager : MonoBehaviour
 {
     public GameObject costume1;
     public GameObject costume2;
+    public GameObject costume3;
+    public GameObject costume4;
+    public GameObject costume5; // Add reference for the fifth costume GameObject
 
     private string costumePlayerPrefsKey = "costume";
 
@@ -21,11 +24,41 @@ public class CostumeManager : MonoBehaviour
         {
             costume1.SetActive(true);
             costume2.SetActive(false);
+            costume3.SetActive(false);
+            costume4.SetActive(false);
+            costume5.SetActive(false);
         }
         else if (selectedCostume == 1)
         {
             costume1.SetActive(false);
             costume2.SetActive(true);
+            costume3.SetActive(false);
+            costume4.SetActive(false);
+            costume5.SetActive(false);
+        }
+        else if (selectedCostume == 2)
+        {
+            costume1.SetActive(false);
+            costume2.SetActive(false);
+            costume3.SetActive(true);
+            costume4.SetActive(false);
+            costume5.SetActive(false);
+        }
+        else if (selectedCostume == 3)
+        {
+            costume1.SetActive(false);
+            costume2.SetActive(false);
+            costume3.SetActive(false);
+            costume4.SetActive(true);
+            costume5.SetActive(false);
+        }
+        else if (selectedCostume == 4) // Handle the fifth costume
+        {
+            costume1.SetActive(false);
+            costume2.SetActive(false);
+            costume3.SetActive(false);
+            costume4.SetActive(false);
+            costume5.SetActive(true);
         }
     }
 }
